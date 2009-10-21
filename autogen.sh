3c08ba38221985f92f6f228bcb28f481cdcae484
Ali Abdallah <ali@ali-xfce.org>
2009-10-21 11:54:32 +0200

    Get rid of the LINGUAS file

--- autogen.sh
+++ autogen.sh
@@ -1,53 +1,20 @@
 #!/bin/sh
 #
-# vi:set et ai sw=2 sts=2 ts=2: */
-# 
-# Copyright (c) 2007-2009 Jannis Pohlmann <jannis@xfce.org>
-# 
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Library General Public
-# License as published by the Free Software Foundation; either
-# version 2 of the License, or (at your option) any later version.
-# 
-# This library is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
-# GNU Library General Public License for more details.
-# 
-# You should have received a copy of the GNU Library General 
-# Public License along with this library; if not, write to the 
-# Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-# Boston, MA 02110-1301, USA.
+# Copyright (c) 2002-2009
+#         The Xfce development team. All rights reserved.
+#
+# Written for Xfce by Benedikt Meurer <benny@xfce.org>.
+
+export XDT_AUTOGEN_REQUIRED_VERSION="4.7.0"
 
 (type xdt-autogen) >/dev/null 2>&1 || {
   cat >&2 <<EOF
 autogen.sh: You don't seem to have the Xfce development tools installed on
             your system, which are required to build this software.
-            Please install the xfce4-dev-tools package first, it is available
-            from http://www.xfce.org/.
+            Please install the xfce4-dev-tools package first, available from
+            http://xfce.org/~benny/projects/xfce4-dev-tools/.
 EOF
   exit 1
 }
 
-# Verify that po/LINGUAS is present
-(test -f po/LINGUAS) >/dev/null 2>&1 || {
-  cat >&2 <<EOF
-autogen.sh: The file po/LINGUAS could not be found. Please check your snapshot
-            or try to checkout again.
-EOF
-  exit 1
-}
-
-# Substitute revision and linguas
-linguas=`sed -e '/^#/d' po/LINGUAS`
-if test -d .git; then
-  revision=$(git rev-parse --short HEAD 2>/dev/null)
-else
-  revision="Unknown"
-fi
-sed -e "s/@LINGUAS@/${linguas}/g" \
-    -e "s/@REVISION@/${revision}/g" \
-    < "configure.ac.in" > "configure.ac"
-
-exec xdt-autogen $@
-
+xdt-autogen $@
