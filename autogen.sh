eb5df914c640ada97f8f5dca0b2b350a45246bce
Christoph Mende <mende.christoph@gmail.com>
2012-07-20 11:25:54 +0200

    Use xdt-csource instead of exo-csource

--- autogen.sh
+++ autogen.sh
@@ -5,7 +5,7 @@
 #
 # Written for Xfce by Benedikt Meurer <benny@xfce.org>.
 
-export XDT_AUTOGEN_REQUIRED_VERSION="4.7.0"
+export XDT_AUTOGEN_REQUIRED_VERSION="4.9.0"
 
 (type xdt-autogen) >/dev/null 2>&1 || {
   cat >&2 <<EOF
