f9e8ef589a0530c6f4bfadfc5fbf990a66277e8a
Sean Davis <smd.seandavis@gmail.com>
2013-12-11 19:48:50 -0500

    Update licenses

--- src/plugins/tray/tray-plugin.c
+++ src/plugins/tray/tray-plugin.c
@@ -1,5 +1,7 @@
 /*
  * * Copyright (C) 2009-2011 Ali <aliov@xfce.org>
+ * * Copyright (C) 2012-2013 Sean Davis <smd.seandavis@gmail.com>
+ * * Copyright (C) 2012-2013 Simon Steinbeiß <ochosi@xfce.org
  *
  * Licensed under the GNU General Public License Version 2
  *
