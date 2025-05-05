03c9906c53a8c088e7335e8ae1b72d3171e0540a
Bobby Rong <rjl931189261@126.com>
2025-05-05 08:01:39 +0800

    build: Fix shellcheck issues in mime scripts
    SC2046 (warning): Quote this to prevent word splitting.
    SC2006 (style): Use $(...) notation instead of legacy backticks `...`.
    SC2086 (info): Double quote to prevent globbing and word splitting.
    
    Since meson passes absolute paths, not quoting variables can lead to
    empty audio_mime_types and video_mime_types.
    
    Also silence "grep: warning: stray \ before /".

--- data/mime/mime-functions.sh
+++ data/mime/mime-functions.sh
@@ -2,13 +2,13 @@
 
 get_audio_mimetypes ()
 {
-	MIMETYPES=`grep -v '^#' $1 | grep "\/" | grep audio | grep -v "audio/x-pn-realaudio" | grep -v "audio/x-scpls" | grep -v "audio/mpegurl" | grep -v "audio/x-mpegurl" | grep -v x-scheme-handler/`
+	MIMETYPES=$(grep -v '^#' "$1" | grep "/" | grep audio | grep -v "audio/x-pn-realaudio" | grep -v "audio/x-scpls" | grep -v "audio/mpegurl" | grep -v "audio/x-mpegurl" | grep -v x-scheme-handler/)
 	MIMETYPES="$MIMETYPES application/x-flac audio/x-pn-realaudio"
 }
 
 get_video_mimetypes ()
 {
-	MIMETYPES=`grep -v '^#' $1 | grep -v x-content/ | grep -v audio | grep -v "application/x-flac" | grep -v "text/google-video-pointer" | grep -v "application/x-quicktime-media-link" | grep -v "application/smil" | grep -v "application/smil+xml" | grep -v "application/x-smil" | grep -v "application/xspf+xml" | grep -v x-scheme-handler/`
+	MIMETYPES=$(grep -v '^#' "$1" | grep -v x-content/ | grep -v audio | grep -v "application/x-flac" | grep -v "text/google-video-pointer" | grep -v "application/x-quicktime-media-link" | grep -v "application/smil" | grep -v "application/smil+xml" | grep -v "application/x-smil" | grep -v "application/xspf+xml" | grep -v x-scheme-handler/)
 	MIMETYPES="$MIMETYPES application/vnd.rn-realmedia"
 }
 
